36a79d9580c8f42d21129f2148398b89db17b056
vesslina <vessslina@gmail.com>
2026-02-28 15:31:41 +0300

    New update 0.2.9

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,22 +4,26 @@ import kotlin.random.Random
 var gameNumber: String = "День 1 "
 var gameHour: String = "- 07:20 AM -"
 var gameDay: String = "Понедельник"
+
+
+var boshki = false  // false, димон не курил бошки и завтракал тухлыми яйцами либо вообще не завтракал
+var sral = false  //срал димон в уровне "Утро" или нет
+var eating = false //Ел ли димон тухлые яйца
+var dimonProebalTralik: Boolean = false  // true - если димон проебал тралик и ехал на чем-то другом в колледж
+var taxiBoshki = false // Ехал ли димон накуренный на такси
+var taxi = false // Ехал ли димон на такси
+
+
 fun main(){
-
-
-
-//ВЫЗОВ ФУНКЦИЙ
-    clearConsole()
-    if (!intro() ){ // Начало игры/приветствие
-        return}
+     //ВЫЗОВ ФУНКЦИЙ
+    clearConsole()// Чистит консоль для красоты
+    intro() // Интро игры
 
     if (!morning()){ // Начало игры/Утро
         return
     }
 
-    if (!introSecond()){
-        return
-    }
+   introSecond()//Интро после прохождения уровня "утро"
 
     if (!morningLunch()) { //Начало игры/ утро - обед
         return
---
+++ src/main/kotlin/TEST/while.kt
@@ -0,0 +1,12 @@
+package TEST
+
+//Тест кода
+fun main(){
+var a = 1
+    a +=2
+    println(a)
+
+    }
+
+
+
